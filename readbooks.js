210f9157f242ecf072e9b70e6ae936309afaefb7
Knut Ole Kvilhaug Magnussen <komagnus@hiof.no>
2023-02-23 17:11:50 +0100

    la til duplicate-sjekk

--- readbooks.js
+++ readbooks.js
@@ -1,19 +1,26 @@
 let number = 0;
+let inputs = [];
 
 function addItem() {
     number++
     var input = document.getElementById("inputField");
     var inputValue = input.value;
-    var li = document.createElement("li");
-    li.setAttribute('id', 'li' + number);
-    var text = document.createTextNode(inputValue);
-    li.appendChild(text);
-    var ul = document.getElementById("list");
-    ul.appendChild(li);
-    input.value = "";
-    li.addEventListener('copy', function(event) {
-      li.style.textDecoration = 'line-through';
-  });
+    if (inputs.includes(inputValue)) {
+      alert("Boken er allerede i listen")
+    } else {
+      inputs.push(inputValue)
+      var li = document.createElement("li");
+      li.setAttribute('id', 'li' + number);
+      var text = document.createTextNode(inputValue);
+      li.appendChild(text);
+      var ul = document.getElementById("list");
+      ul.appendChild(li);
+      input.value = "";
+      li.addEventListener('copy', function(event) {
+        li.style.textDecoration = 'line-through';
+    });
+    }
+   
   }
   
  
